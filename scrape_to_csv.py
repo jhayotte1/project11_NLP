b2f933ae82d7789f2451e83bea522fca2e3567ab
irlg0at <34748847+irlg0at@users.noreply.github.com>
2025-10-27 22:01:51 +0100

    scrape_to_csv.py: Better handling of cleaning, further scraping implementation, finished CSV files

--- scrape_to_csv.py
+++ scrape_to_csv.py
@@ -1,11 +1,13 @@
 import os
 import csv
+import sys
 from df_constr import load_poetry_ids
 from rdflib import Graph
 from rdflib.namespace import DCTERMS
 import requests
 from tqdm import tqdm
 
+csv.field_size_limit(sys.maxsize)
 
 CACHE_DIR = "data/cache"
 LOVE_IDS_PATH = os.path.join(CACHE_DIR, "love_ids.json")
@@ -58,7 +60,7 @@ def download_text(id, url, save_dir="data/texts"):
         response.raise_for_status()
         with open(save_path, "w", encoding="utf-8") as f:
             f.write(response.text)
-        print(f"Saved ebook {id}.txt")
+        #print(f"Saved ebook {id}.txt")
         return save_path
     except Exception as e:
         print(f"Error downloading ebook {id}: {e}")
@@ -68,9 +70,20 @@ def download_text(id, url, save_dir="data/texts"):
 def clean_ebook(id: int, ebook_dir: str = "data/texts") -> (str, str, str, str):
     TITLE_STRING = "Title: "
     AUTHOR_STRING = "Author: "
+    CONTRIBUTOR_STRING = "Contributor: "
+    TRANSLATOR_STRING = "Translator: "
+    CREATOR_STRING = "Creator: "
+    ILLUSTRATOR_STRING = "Illustrator: "
     RELEASE_STRING = "Release date: "
+    COMPILER_STRING = "Compiler: "
+
     START_STRING = "*** START OF THE PROJECT GUTENBERG EBOOK"
     END_STRING = "*** END OF THE PROJECT GUTENBERG EBOOK"
+    EDITOR_STRING = "Editor: "
+    
+    txt_path = os.path.join(ebook_dir, f"{id}.txt")
+    if not os.path.exists(txt_path):
+        return None
 
     with open(os.path.join(ebook_dir, f"{id}.txt"), "r") as ebook:
         title = ""
@@ -86,6 +99,24 @@ def clean_ebook(id: int, ebook_dir: str = "data/texts") -> (str, str, str, str):
         for line in ebook:
             if line.startswith(AUTHOR_STRING):
                 author = line[8:].strip("\n")
+                break
+            elif line.startswith(CONTRIBUTOR_STRING):
+                author = line[13:].strip("\n")
+                break
+            elif line.startswith(TRANSLATOR_STRING):
+                author = line[12:].strip("\n")
+                break
+            elif line.startswith(EDITOR_STRING):
+                author = line[8:].strip("\n")
+                break
+            elif line.startswith(CREATOR_STRING):
+                author = line[9:].strip("\n")
+                break
+            elif line.startswith(COMPILER_STRING):
+                author = line[10:].strip("\n")
+                break
+            elif line.startswith(ILLUSTRATOR_STRING):
+                author = line[13:].strip("\n")
                 break
 
         for line in ebook:
@@ -116,38 +147,54 @@ def add_to_csv(
     os.makedirs(csv_dir, exist_ok=True)
     csv_path = os.path.join(csv_dir, csv_file)
 
-    with open(csv_path, "w") as csvfile:
+    with open(csv_path, "a", newline='') as csvfile:
         write = csv.writer(csvfile)
         write.writerow((id, *tart))
 
 
+
+def load_existing_ids(csv_file: str, csv_dir:str = CSV_DIR) -> list:
+    skip = []
+    with open(CSV_DIR + f"/{csv_file}", "r", newline='') as csvfile:
+        read = csv.reader(csvfile)
+        for row in read:
+            skip.append(row[0])
+    return skip
+
 if __name__ == "__main__":
     generate = True
+    skip = []
     if os.path.exists(CSV_DIR + "/love.csv"):
         if not (input("CSV file for love found, do you want to regenerate it? Y/N: ").lower().strip() == "y"):
             generate = False
-                
+        skip = load_existing_ids("love.csv")  
+    
     if generate:
         print("Downloading and parsing love texts...")
         for id in tqdm(love_ids):
-            url = get_url_for_download(id)
-            saved_path = download_text(id,url)
-            add_to_csv(id,clean_ebook(id),"love.csv")
-
+            if id not in skip:
+                url = get_url_for_download(id)
+                saved_path = download_text(id,url)
+                cleaned = clean_ebook(id)
+                if cleaned is not None:
+                    add_to_csv(id,clean_ebook(id),"love.csv")
+    
     generate = True
+    skip = []
     if os.path.exists(CSV_DIR + "/hate.csv"):
         if not (input("CSV file for hate found, do you want to regenerate it? Y/N: ").lower().strip() == "y"):
             generate = False
-                
+        skip = load_existing_ids("hate.csv")  
+    
     if generate:
         print("Downloading and parsing hate texts...")
         for id in tqdm(hate_ids):
-            url = get_url_for_download(id)
-            saved_path = download_text(id,url)
-            add_to_csv(id,clean_ebook(id),"hate.csv")
-        
-
-
+            if id not in skip:
+                url = get_url_for_download(id)
+                saved_path = download_text(id,url)
+                cleaned = clean_ebook(id)
+                if cleaned is not None:
+                    add_to_csv(id,clean_ebook(id),"hate.csv")
 
 
 ##Test
