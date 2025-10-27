ff5c6bd983d5c17b30b65764bcebf63d3ed445ab
jhayotte1 <jhayotte2004@gmail.com>
2025-10-27 18:11:35 +0200

    Get url link to text from rdf files

--- scrape_to_csv.py
+++ scrape_to_csv.py
@@ -1,11 +1,14 @@
-import os
+import os, glob
 from df_constr import load_poetry_ids
+from rdflib import Graph, Namespace
+from rdflib.namespace import DCTERMS, RDF
 import requests
 
 
 CACHE_DIR = "data/cache"
 LOVE_IDS_PATH = os.path.join(CACHE_DIR, "love_ids.json")
 HATE_IDS_PATH = os.path.join(CACHE_DIR, "hate_ids.json")
+RDF_BASE_DIR = "gutenberg_rdf/cache/epub"
 
 if os.path.exists(LOVE_IDS_PATH) and os.path.exists(HATE_IDS_PATH):
     hate_ids = load_poetry_ids(HATE_IDS_PATH)
@@ -15,4 +18,27 @@ else:
     raise FileNotFoundError(
         "LOVE and HATE cache files not found, please run df_constr.py first"
     )
+
+def id_to_path(id):
+    return os.path.join(RDF_BASE_DIR, str(id), f"pg{id}.rdf")
+
+def get_url_for_download(id):
+    rdf_path = id_to_path(id)
+
+    if not os.path.exists(rdf_path):
+        print(f"Error: this id ({id}) does not have a corresponding file")
+        return None
+    g = Graph()
+
+    try :
+        g.parse(rdf_path)
+    except Exception as e:
+        print(f"Error parsing RDF for id {id}: {e}")
+        return None
+    
+    for file_node in g.objects(None, DCTERMS.hasFormat):
+        url = str(file_node)
+        if url.endswith(f"{id}.txt.utf-8"):
+            return url
+    return None
 
