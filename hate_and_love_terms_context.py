3ce1af1e91f07fcb21749df299e9cfb04fe1f1e9
jhayotte1 <jhayotte2004@gmail.com>
2025-11-02 00:43:14 +0200

    H&L context : improved implementation

--- hate_and_love_terms_context.py
+++ hate_and_love_terms_context.py
@@ -1,12 +1,17 @@
 import os
+import spacy
+nlp = spacy.load("en_core_web_sm", disable=["parser", "ner", "textcat"])
+nlp.max_length = 20_000_000
+
 import pandas as pd
 import matplotlib.pyplot as plt
-import spacy
+from spacy.matcher import Matcher
 from pandas import read_csv
 from dataframe_stat import tokenize_text
-from nltk.stem import WordNetLemmatizer
-from nltk.stem.porter import *
 from wordcloud import WordCloud
+from typing import Iterable, List, Tuple, Set, Dict
+from collections import defaultdict, Counter
+from tqdm import tqdm
 
 
 # Collected from: https://wordnik.com/words/hate 
@@ -74,124 +79,124 @@ love_vocab = {
     'young man', 'zeal'
 }
 
-nlp = spacy.load("en_core_web_sm", disable=["parser", "ner", "textcat"])
-nlp.max_length = 6_000_000
+
+
 
 CSV_DIR = "data/csv"
-lemmatizer = WordNetLemmatizer()
+
 
 #Vocab preparation
-def prepare_vocab(vocab, nlp):
-    singles = set()
-    phrases = set()
-    max_len = 1
+def prepare_vocab(vocab_raw: Iterable[str], nlp_model) -> Tuple[Set[str], Set[Tuple[str, ...]], int]:
+    prepared = set()
     
-    for expr in vocab:
-        doc = nlp(expr)
-        lems = [t.lemma_.lower() for t in doc if not (t.is_punct or t.is_space)]
+    for expr in vocab_raw:
+        doc = nlp_model(expr)
+        lems = tuple(t.lemma_.lower() for t in doc if not (t.is_punct or t.is_space))
         if not lems:
             continue
-        if len(lems) == 1:
-            singles.add(lems[0])
-        else:
-            phrases.add(" ".join(lems))
-            if len(lems) > max_len:
-                max_len = len(lems)
-    return singles, phrases, max_len
-
-love_singles_vocab, love_phrases_vocab, love_max_len_vocab = prepare_vocab(love_vocab, nlp)
-hate_singles_vocab, hate_phrases_vocab, hate_max_len_vocab = prepare_vocab(hate_vocab, nlp)
+        prepared.add(lems)
+    return prepared
 
 
+def build_matcher_from_lemmas(lemmas_patterns: Set[Tuple[str, ...]], nlp_model) -> Matcher:
+    m = Matcher(nlp_model.vocab)
+    patterns = [[{"LEMMA": lemma} for lemma in lemmas] for lemmas in lemmas_patterns]
+    if patterns:
+        m.add("TARGET", patterns)
+    return m
 
+def iter_doc_chunk(text: str, nlp_model, max_tokens: int = 50_000, overlap: int = 2):
+    base_doc = nlp_model(text)
+    N = len(base_doc)
+    if N==0:
+        return
+    i = 0
+    while i < N:
+        j = min(i+max_tokens, N)
+        usable_start = i + (overlap if i > 0 else i)
+        usable_end = j - (overlap if j < N else j)
+        subtext = base_doc[i:j].text
+        yield subtext, i, j, usable_start, usable_end
+        i = j - overlap
 
-
-def lemmatize_text(text: str) -> list:
-    doc = nlp(text)
-    lemmed = [token.lemma_.lower() for token in doc if not (token.is_punct or token.is_space)]
-    return lemmed
-
-
-def hate_context_in_text(lemmed_text: list) -> list:
-    cont = []
-    n=len(lemmed_text)
-
-    for i, word in enumerate(lemmed_text):
-        if word in hate_singles_vocab:
-            for off in (-2, -1, 1, 2):
-                if i + off >= 0 and i + off < len(lemmed_text):
-                    cont.append(lemmed_text[i + off])
-        for L in range(min(love_max_len_vocab, n-i), 1, -1):
-            span = tuple(lemmed_text[i:i+L])
-            if span in love_phrases_vocab:
-                start, end = i, i+L-1
-                for k in (start-2, start-1, end+1, end+2):
-                    if k >= 0 and k < n:
-                        cont.append(lemmed_text[k])
-                break
-    cont = set(cont)
-    return cont
-
-
-def love_context_in_text(lemmed_text: list) -> list:
-    cont = []
-    n = len(lemmed_text)
-    
-    for i, word in enumerate(lemmed_text):
-        if word in love_singles_vocab:
-            for off in (-2, -1, 1, 2):
-                k = i + off
+def count_context_with_matcher(texts, matcher, nlp_model):
+    cnt = Counter()
+    for text in tqdm(texts, desc="Processing books", unit="book"):
+        if not isinstance(text, str) or not text:
+            continue
+        doc = nlp_model(text)  # un livre complet
+        n = len(doc)
+        matches = matcher(doc)
+        for _, start, end in matches:           # end exclusif
+            for k in (start-2, start-1, end, end+1):
                 if 0 <= k < n:
-                    cont.append(lemmed_text[k])
-        for L in range(min(love_max_len_vocab, n - i), 1, -1):  
-            span = tuple(lemmed_text[i:i+L])
-            if span in love_phrases_vocab:
-                start, end = i, i+L-1
-                for k in (start-2, start-1, end+1, end+2):
-                    if 0 <= k < n:
-                        cont.append(lemmed_text[k])
-                break
-    cont = set(cont)
-    return cont
+                    tok = doc[k]
+                    if not (tok.is_space or tok.is_punct or tok.is_stop):
+                        cnt.update([tok.lemma_.lower()])
+    return dict(cnt)
 
 
-def love_cont_freq_df(love_context: set, lemmed_texts: list) -> dict:
-    freq = {}
-    for word in love_context:
-        freq[word] = sum(text.count(word) for text in lemmed_texts)
-    return freq
+def count_context_with_matcher_chunked(texts, matcher: Matcher, nlp_model, max_tokens: int = 50_000, overlap=2):
+    cnt = Counter()
+    for text in tqdm(texts, desc="Processing (chunked)", unit="book"):
+        if not isinstance(text, str) or not text:
+            continue
+        for subtext, start_tok, end_tok, usable_start, usable_end in iter_doc_chunk(text, nlp_model, max_tokens, overlap):
+            doc = nlp_model(subtext)
+            n = len(doc)
+            matches = matcher(doc)
+        
+            for _, start, end in matches:
+                g_s = start_tok + start
+                g_e = start_tok + end
 
-def hate_cont_freq_df(hate_context: set, lemmed_texts: list) -> dict:
-    freq = {}
-    for word in hate_context:
-        freq[word] = sum(text.count(word) for text in lemmed_texts)
-    return freq
+                if not (usable_start <= g_s and g_e <= usable_end):
+                    continue
+
+                for off in (end, end+1, start-1, start-2):
+                    if 0 <= off < n:
+                        tok=doc[off]
+                        if not (tok.is_punct or tok.is_space or tok.is_stop):
+                            cnt.update([tok.lemma_.lower()])
+    return cnt
 
 
-def plot_wordcloud(freq_dict: dict, title: str):
-    wc = WordCloud(width=1000, height=500, background_color='white', colormap='viridis').generate_from_frequencies(freq_dict)
-    plt.figure(figsize=(15, 7.5))
+def plot_wordcloud(freq_dict: dict, title: str, top_n: int | None = None):
+    if top_n is not None and top_n < len(freq_dict) and top_n > 0:
+        freq_dict = dict(sorted(freq_dict.items(), key=lambda item: item[1], reverse=True)[:top_n])
+    
+    wc = WordCloud(width=1200, height=600, background_color='white', colormap='viridis').generate_from_frequencies(freq_dict)
+    plt.figure(figsize=(12, 6))
     plt.imshow(wc, interpolation='bilinear')
     plt.axis('off')
     plt.title(title, fontsize=20)
+    plt.savefig(os.path.join("img", f"{title.lower().replace(' ', '_')}_wordcloud.png"))
     plt.show()
+
+
 
 if __name__ == "__main__":
     love = read_csv(os.path.join(CSV_DIR, "love.csv"))
     hate = read_csv(os.path.join(CSV_DIR, "hate.csv"))
     love_texts,hate_texts = love.iloc[:,4],hate.iloc[:,4]
     
-    love_lemmed = [lemmatize_text(text) for text in love_texts]
-    hate_lemmed = [lemmatize_text(text) for text in hate_texts]
+    love_patterns = prepare_vocab(love_vocab, nlp)
+    love_matcher = build_matcher_from_lemmas(love_patterns, nlp)
 
-    love_context = [love_context_in_text(t) for t in love_lemmed]
-    hate_context = [hate_context_in_text(t) for t in hate_lemmed]
-    #remove duplicates
-    love_context = set(word for context in love_context for word in context)
-    hate_context = set(word for context in hate_context for word in context)
+    try:
+        love_freq = count_context_with_matcher(love_texts, love_matcher, nlp)
+    except Exception as e:
+        print("Chunked book")
+        love_freq = count_context_with_matcher_chunked(love_texts, love_matcher, nlp, max_tokens=50_000, overlap=2)
 
-    love_context_freq_in_love = love_cont_freq_df(love_context, love_lemmed)
-    hate_context_freq_in_love = hate_cont_freq_df(hate_context, love_lemmed)
+    hate_patterns = prepare_vocab(hate_vocab, nlp)
+    hate_matcher = build_matcher_from_lemmas(hate_patterns, nlp)
 
-    plot_wordcloud(love_context_freq_in_love, "Love Context in Love Texts")
-    plot_wordcloud(hate_context_freq_in_love, "Hate Context in Love Texts")
+    try:
+        hate_freq = count_context_with_matcher(hate_texts, hate_matcher, nlp)
+    except Exception as e:
+        print("Chunked book")
+        hate_freq = count_context_with_matcher_chunked(hate_texts, hate_matcher, nlp, max_tokens=50_000, overlap=2)
+
+    plot_wordcloud(love_freq, "Love Context Word Cloud", top_n = 200)
+    plot_wordcloud(hate_freq, "Hate Context Word Cloud", top_n = 200)
